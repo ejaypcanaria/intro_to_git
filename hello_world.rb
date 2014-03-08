d47a1321995f53ae158df5c014f6a8e5f1dcf72b
Ejay Canaria <ejaypcanaria@gmail.com>
2014-03-08 19:52:01 +0800

    Refactor say_hello_to to greet

--- hello_world.rb
+++ hello_world.rb
@@ -2,9 +2,9 @@ def hello_world
   puts "Hello world from Ruby!"
 end
 
-def say_hello_to(name)
-  puts "Hello, #{name}!"
+def greet(salut, name)
+  puts "#{salut}, #{name}!"
 end
 
 hello_world
-say_hello_to("Ejay")
+greet("Ejay")
