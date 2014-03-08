42110726a513fada571a67d0cf773d4a5a437bd8
Ejay Canaria <ejaypcanaria@gmail.com>
2014-03-08 19:50:20 +0800

    Change say_hello_to to say_hi_to

--- hello_world.rb
+++ hello_world.rb
@@ -2,9 +2,9 @@ def hello_world
   puts "Hello world from Ruby!"
 end
 
-def say_hello_to(name)
-  puts "Hello, #{name}!"
+def say_hi_to(name)
+  puts "Hi, #{name}!"
 end
 
 hello_world
-say_hello_to("Ejay")
+say_hi_to("Ejay")
